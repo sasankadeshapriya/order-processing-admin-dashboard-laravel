1cf2f4c3051207ad2be558c1fa5ff59c651ebc28
DilharaSannasgala <99758031+DilharaSannasgala@users.noreply.github.com>
2024-06-13 10:58:42 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -159,11 +159,6 @@ Route::middleware(['web'])->group(function () {
     //Route for sales report
     Route::get('/sales-report', [ReportController::class, 'showSales'])->name('sales.show');
     Route::get('/api/sales/report', [ReportController::class, 'getSalesReport']);
-
-    //tracking..
-    Route::get('/tracking', [AssignmentController::class, 'showTodayAssignments'])->name('emp.tracking');
-    Route::get('/employee/{employeeId}/location', [AssignmentController::class, 'getEmployeeLocation']);
-
 });
 
 
